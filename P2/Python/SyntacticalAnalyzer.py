c63c3266d8e7db6e537bc8dac9ea71d4e2b5b145
JustinR840 <ramosjus@sonoma.edu>
2018-04-21 16:12:09 -0700

    Created helpers file, moved firsts and follows tables to there

--- P2/Python/SyntacticalAnalyzer.py
+++ P2/Python/SyntacticalAnalyzer.py
@@ -4,25 +4,6 @@ from Tokens import Token
 
 class SyntacticalAnalyzer(object):
 
-	# EOF_T, LPAREN_T, DEFINE_T, IDENT_T, RPAREN_T, NUMLIT_T, STRLIT_T, QUOTE_T, ELSE_T, IF_T, COND_T, LISTOP_T, CONDS_T, AND_T, OR_T, NOT_T, NUMBERP_T, SYMBOLP_T, LISTP_T, ZEROP_T, NULLP_T, STRINGP_T, PLUS_T, MINUS_T, DIV_T, MULT_T, MODULO_T, EQUALTO_T, GT_T, LT_T, GTE_T, LTE_T, IDENT_T, DISPLAY_T, NEWLINE_T
-
-	firsts = [
-		[],
-		[],
-		[],
-		[],
-		[],
-		[],
-		[],
-		[],
-		[],
-		[],
-		[],
-		[],
-		[],
-		[]
-	]
-	follows = [[],[],[]]
 
 
 
